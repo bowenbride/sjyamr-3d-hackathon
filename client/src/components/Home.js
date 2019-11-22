f8e33cb7413d0831de4216927c390188ba407ffd
jennormous <smj.boudreau@gmail.com>
2019-11-22 13:34:03 -0800

    added BrowserRouter and created cities component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
-import Hero from './home/Hero'
-import Chart from './Chart'
+import React, { Component } from "react";
+import {Switch, Route} from "react-router-dom"
+import Hero from "./home/Hero";
+import Cities from "./home/Cities";
+import Chart from "./Chart";
+
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Hero />
-                <Chart />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+          <Switch>
+        <Hero />
+        <Cities />
+        <Chart />
+        </Switch>
+      </div>
+    );
+  }
 }

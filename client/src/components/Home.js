e08769f547e2cf2d06a000eb372c51df027bd085
jennormous <smj.boudreau@gmail.com>
2019-11-22 14:12:39 -0800

    added assets for city and word map

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 import Hero from "./home/Hero";
 import Cities from "./home/Cities";
 import Chart from "./Chart";
-
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-          <Switch>
         <Hero />
         <Cities />
         <Chart />
-        </Switch>
       </div>
     );
   }
--- client/src/components/home/Cities.js
+++ client/src/components/home/Cities.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import newdel from "../../assets/city-photo/newdel.jpg";
+import noida from "../../assets/city-photo/noida.jpg";
+import gurgaon from "../../assets/city-photo/gurgaon.jpg";
 
 export default class Cities extends Component {
   render() {
-
-
     return (
-      <div>
-        <h1>Choose City</h1>
+      <div className="select-city">
+        <h1 className="select-city__h1">Choose City</h1>
         <div className="img-container">
-          <img src alt="new delhi">
-          </img>
-          <img src alt="gurgaon">
-          </img>
-          <img src alt="some city">
-          </img>
+          <img className="city-img" src={newdel} alt="new delhi"></img>
+          <img className="city-img" src={noida} alt="gurgaon"></img>
+          <img className="city-img" src={gurgaon} alt="some city"></img>
         </div>
       </div>
-    )
+    );
   }
 }
